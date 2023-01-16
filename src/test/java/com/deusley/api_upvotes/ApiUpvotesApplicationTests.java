402765ae3191d5a1164f2de70d8aebc9bec92dee
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-01-16 16:19:07 -0300

    firstCommit

---
+++ src/test/java/com/deusley/api_upvotes/ApiUpvotesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deusley.api_upvotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiUpvotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
