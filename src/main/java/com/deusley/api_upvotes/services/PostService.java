8446e5d99c20ec705c415621be8b63f4e512fc54
Deuslleyw <94429495+Deuslleyw@users.noreply.github.com>
2023-01-18 01:31:05 -0300

    implServcess

---
+++ src/main/java/com/deusley/api_upvotes/services/PostService.java
@@ -0,0 +1,30 @@
+package com.deusley.api_upvotes.services;
+
+import com.deusley.api_upvotes.domain.Post;
+import com.deusley.api_upvotes.dto.PostDTO;
+import com.deusley.api_upvotes.repositories.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository rep;
+
+    @Transactional(readOnly = true)
+    public Page<PostDTO> findAll(Pageable pageable) {
+        Page<Post> result = rep.findAll(pageable);
+        Page<PostDTO> pageDdto = result.map(PostDTO::new);
+
+        return pageDdto;
+
+    }
+
+
+}
